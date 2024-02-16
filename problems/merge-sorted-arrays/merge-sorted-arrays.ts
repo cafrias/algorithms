6bd7e180e60e75f5c65a8aaaf24707e422faa1c6
Carlos Frias <carlos.a.frias@gmail.com>
2024-02-16 06:37:42 -0300

    fix: improve merge sorted array algo

--- problems/merge-sorted-arrays/merge-sorted-arrays.ts
+++ problems/merge-sorted-arrays/merge-sorted-arrays.ts
@@ -4,24 +4,18 @@ export function mergeSortedArrays(
   nums2: number[],
   n: number
 ): void {
-	let i = m - 1;
-	let j = n - 1;
-	let k = n + m - 1;
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
 
-	while(i >= 0 && j >= 0) {
-		if(nums1[i] > nums2[j]) {
-			nums1[k] = nums1[i];
-			i--;
-		} else {
-			nums1[k] = nums2[j];
-			j--;
-		}
-		k--;
-	}
-
-	while(j >= 0) {
-		nums1[k] = nums2[j];
-		k--;
-		j--;
-	}
+  while (k > i) {
+    if (i > -1 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
 }
